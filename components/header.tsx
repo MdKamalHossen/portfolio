2dc92e8ea701076fe27a44ff1eb384a0e32c9199
Kamal Hossen <kamalbubt63@gmail.com>
2022-01-09 20:49:20 +0600

    name

--- components/header.tsx
+++ components/header.tsx
@@ -22,7 +22,7 @@ const Header = () => {
       <nav className={styles.nav}>
         <div className={styles.circleWrap} style={{ background: theme.sidebarLeftBg }}>
           <Link href="/">
-            <a><img src=''</a>
+            <a>Kamal</a>
           </Link>
         </div>
         <ul className={theme?.borders ? 'noBorder' : ''}>
